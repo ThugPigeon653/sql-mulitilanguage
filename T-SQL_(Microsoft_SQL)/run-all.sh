987860c9f82bf6bec05db69f3399ac4bd0d08272
ThugPigeon653 <89567974+ThugPigeon653@users.noreply.github.com>
2023-11-22 15:42:23 +1000

    using mssql docker image on ubuntu for tsql tests

--- T-SQL_(Microsoft_SQL)/run-all.sh
+++ T-SQL_(Microsoft_SQL)/run-all.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-docker exec -it sql_server_container /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P password -d master -i Create.sql
-docker exec -it sql_server_container /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P password -d master -i Setup.sql
-docker exec -it sql_server_container /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P password -d master -i Insert.sql
-docker exec -it sql_server_container /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P password -d master -i Query.sql
-docker exec -it sql_server_container /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P password -d master -i Drop.sql
+ls
