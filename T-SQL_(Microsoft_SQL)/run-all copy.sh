6f258341becd1070a7bed150221ee51d0cc60857
ThugPigeon653 <89567974+ThugPigeon653@users.noreply.github.com>
2023-11-22 13:26:17 +1000

    using mssql docker image on ubuntu for tsql tests

---
+++ T-SQL_(Microsoft_SQL)/run-all copy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sqlcmd -S %COMPUTERNAME%\MSSQLSERVER01 -P password -i Create.sql
+sqlcmd -S %COMPUTERNAME%\MSSQLSERVER01 -P password -i Setup.sql
+sqlcmd -S %COMPUTERNAME%\MSSQLSERVER01 -P password -i Insert.sql
+sqlcmd -S %COMPUTERNAME%\MSSQLSERVER01 -P password -i Query.sql
+sqlcmd -S %COMPUTERNAME%\MSSQLSERVER01 -P password -i Drop.sql
--- test/test.py
+++ test/test.py
@@ -5,11 +5,11 @@ import os
 class testSQL(unittest.TestCase):
     def test_tsql(self):
         os.chdir("T-SQL_(Microsoft_SQL)")
-        p = Popen("run-all.bat", stdout=PIPE, stderr=PIPE, shell=True)
+        p = Popen("run-all.sh", stdout=PIPE, stderr=PIPE, shell=True)
         stdout, stderr = p.communicate()
         self.assertEqual(stderr, b'')
         os.chdir("..")
-
+        
     def test_oracle(self):
         os.chdir('PLSQL_(Oracle)')
         print("done")
