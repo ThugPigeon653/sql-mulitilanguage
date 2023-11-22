cb6f5d4c1b9a53b7668ed7709e981b483901bc28
ThugPigeon653 <89567974+ThugPigeon653@users.noreply.github.com>
2023-11-22 15:24:37 +1000

    using mssql docker image on ubuntu for tsql tests

--- test/test.py
+++ test/test.py
@@ -9,6 +9,8 @@ class testSQL(unittest.TestCase):
         print("-----------------------")
         print(os.listdir(os.curdir))
         os.chdir("T-SQL_(Microsoft_SQL)")
+        
+        print(os.listdir(os.curdir))
         p = Popen("run-all.sh", stdout=PIPE, stderr=PIPE, shell=True)
         stdout, stderr = p.communicate()
         self.assertEqual(stderr, b'')
