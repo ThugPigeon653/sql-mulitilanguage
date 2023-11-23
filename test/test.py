e29fcca38c8a9ad2ef275b80cd70665dafea7290
ThugPigeon653 <89567974+ThugPigeon653@users.noreply.github.com>
2023-11-23 12:19:51 +1000

    modified unit tests

--- test/test.py
+++ test/test.py
@@ -13,7 +13,7 @@ class testSQL(unittest.TestCase):
         cls.connection = pyodbc.connect(
             'Trusted_Connection=Yes;'
             'Driver={ODBC Driver 17 for SQL Server};'
-            'Server=sql-server;'
+            'Server=sql-server,1433;'
             'PWD=password'
         )
         cls.cursor = cls.connection.cursor()
