9c8ed9f42b9262acd47fd28fe9d5a1bd4e1035b7
ThugPigeon653 <89567974+ThugPigeon653@users.noreply.github.com>
2023-11-23 11:44:13 +1000

    modified unit tests

--- test/test.py
+++ test/test.py
@@ -10,14 +10,27 @@ class testSQL(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.connection = pyodbc.connect(
-            Trusted_Connection='Yes',
-            Driver='{ODBC Driver 17 for SQL Server}',
-            Server='sql-server',
-            UID='sa',
-            PWD='password'
-        )
-        cls.cursor = cls.connection.cursor()
+        try:
+            cls.connection = pyodbc.connect(
+                Trusted_Connection='Yes',
+                Driver='{ODBC Driver 17 for SQL Server}',
+                Server='sql-server',
+                UID='sa',
+                PWD='password'
+            )
+            cls.cursor = cls.connection.cursor()
+        except:
+            try:
+                cls.connection = pyodbc.connect(
+                    Trusted_Connection='Yes',
+                    Driver='{ODBC Driver 17 for SQL Server}',
+                    Server='localhost',
+                    UID='sa',
+                    PWD='password'
+                )
+            except:
+                raise ConnectionError
+                
 
     @classmethod
     def tearDownClass(cls):
