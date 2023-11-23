1535c3c360124ca88343f83d0ad1799810d762bf
ThugPigeon653 <89567974+ThugPigeon653@users.noreply.github.com>
2023-11-23 13:35:46 +1000

    modified unit tests

--- test/test.py
+++ test/test.py
@@ -11,7 +11,7 @@ class testSQL(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        host = 'localhost'
+        host = '127.0.0.1'
         port = 1433  
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex((host, port))
