058b8445326f0d037775ef44560daf2e557ce653
ThugPigeon653 <89567974+ThugPigeon653@users.noreply.github.com>
2023-11-24 15:34:34 +1000

    finished oracle testing framework

---
+++ test/bs.py
@@ -0,0 +1,59 @@
+from subprocess import Popen, PIPE
+import unittest
+import os
+import cx_Oracle
+
+class TestOracleSQL():
+
+    @classmethod
+    def setUpClass(cls):
+        oracle_server = "localhost"
+        oracle_service_name = "ORCL"
+        oracle_user = "SYS"
+        oracle_password = os.getenv("ORACLE_PASSWORD")
+        print(f"Oracle Server: {oracle_server}")
+        print(f"Oracle Service Name: {oracle_service_name}")
+        print(f"Oracle User: {oracle_user}")
+        cls.dsn = cx_Oracle.makedsn(oracle_server, 1521, oracle_service_name)
+        print(f"DSN: {cls.dsn}")
+        cls.connection = cx_Oracle.connect(user=oracle_user, password=oracle_password, dsn=cls.dsn)
+        cls.cursor = cls.connection.cursor()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.cursor.close()
+        cls.connection.close()
+
+    def execute_sql_file(self, sql_file_path, cursor, connection):
+        try:
+            with open(sql_file_path, 'r') as sql_file:
+                sql_script = sql_file.read()
+                cursor.execute(sql_script)
+                if not sql_script.strip().upper().startswith("SELECT"):
+                    connection.commit()
+            return True
+        except Exception as e:
+            print(f"Error executing SQL file {sql_file_path}: {str(e)}")
+            return False
+
+    def test_oracle(self):
+        os.chdir("PLSQL_(Oracle)")
+        oracle_connection = self.connection
+        oracle_cursor = self.cursor
+        self.assertTrue(self.execute_sql_file('Schema.sql', oracle_cursor, oracle_connection))
+        self.assertTrue(self.execute_sql_file('Create.sql', oracle_cursor, oracle_connection))
+        self.assertTrue(self.execute_sql_file('CreateSequence.sql', oracle_cursor, oracle_connection))
+        self.assertTrue(self.execute_sql_file('CreateTrigger.sql', oracle_cursor, oracle_connection))
+        self.assertTrue(self.execute_sql_file('CreateProcedure.sql', oracle_cursor, oracle_connection))
+        self.assertTrue(self.execute_sql_file('CreateFunction.sql', oracle_cursor, oracle_connection))
+        self.assertTrue(self.execute_sql_file('Insert.sql', oracle_cursor, oracle_connection))
+        self.assertTrue(self.execute_sql_file('Query.sql', oracle_cursor, oracle_connection))
+        self.assertTrue(self.execute_sql_file('Drop.sql', oracle_cursor, oracle_connection))
+        oracle_cursor.close()
+        oracle_connection.close()
+        os.chdir("..")
+
+
+a=TestOracleSQL()
+a.setUpClass()
+a.test_oracle()
